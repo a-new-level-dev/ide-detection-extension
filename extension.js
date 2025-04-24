008e35bb8a1b790a48d0bceec375e578cf6aec5d
Metal Gabe <anewlevelmp@gmail.com>
2025-04-24 00:27:22 -0700

    chore(main): [no ci] update `activate` logic to do actual stuff;

--- extension.js
+++ extension.js
@@ -1,36 +1,13 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+function activate() {
+   console.log('Congratulations, "ide-detection-extension" is now active!', {
+      ideName: vscode.env.appName,
+   });
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ide-detection-extension" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ide-detection-extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ide-detection-extension!');
-	});
-
-	context.subscriptions.push(disposable);
+   vscode.commands.executeCommand("setContext", "ideName", vscode.env.appName);
 }
-
-// This method is called when your extension is deactivated
-function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+   activate,
+};
